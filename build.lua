4f0b8e1b618041ffb083dfa7e152795604bcf297
Anexon <anexon@protonmail.com>
2024-01-03 16:46:44 +0000

    feat: setup venv through nvim lazy package manager

---
+++ build.lua
@@ -0,0 +1,45 @@
+local function is_windows()
+    return vim.fn.has("win32") == 1 or vim.fn.has("win64") == 1
+end
+
+local function get_path_separator()
+    return is_windows() and "\\" or "/"
+end
+
+local function get_script_path()
+    local path_sep = get_path_separator()
+    local script_path = debug.getinfo(1).source:match("@?(.*)" .. path_sep)
+
+    return script_path or ""
+end
+
+local function run_command(command)
+    local status, _, code = os.execute(command)
+
+    require('notify').notify('Command: ' .. command .. ' || status: ' .. status, 'info')
+    require('notify').notify('status: ' .. status, 'info')
+
+    if status == false then
+        error("Command failed: " .. command .. ". Exit code: " .. tostring(code))
+    end
+end
+
+local function setup()
+    local python = is_windows() and "python" or "python3"
+    local sep = get_path_separator()
+    local script_path = get_script_path()
+    local venv_path = script_path .. sep .. "python3" .. sep .. "venv"
+
+    -- Create virtual environment if it does not exist.
+    if vim.fn.isdirectory(venv_path) == 0 then
+        run_command(python .. " -m venv " .. venv_path)
+    end
+
+    -- Install requirements via pip
+    local pip_cmd = venv_path .. (is_windows() and sep .. "Scripts" .. sep .. "pip" or sep .. "bin" .. sep .. "pip")
+    local requirements_path = script_path .. sep .. "python3" .. sep .. "requirements.txt"
+
+    run_command(pip_cmd .. " install -r " .. requirements_path)
+end
+
+setup()
